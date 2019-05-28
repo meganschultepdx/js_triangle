5c637ce4e7620c56a94299a8cd60cb4748cd6a15
brian hensley and megan schulte <brian@>
2019-05-28 10:33:15 -0700

    convert triangle to use jasmine and karma

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-import { triangle} from './triangle';
+// import { triangle } from './triangle';
 import './styles.css';
 import $ from 'jquery';
 import 'bootstrap';
@@ -6,13 +6,22 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 $(document).ready(function(){
   $("form#calculator").submit(function(event){
-    event.preventDefault();
     var firstInput = parseInt($("#input1").val());
     var secondInput = parseInt($("#input2").val());
     var thirdInput = parseInt($("#input3").val());
-    console.log(typeof firstInput);
-    console.log(secondInput);
-    console.log(thirdInput);
     $("body").find("h4").hide();
+
+    if ( firstInput <= 0 || secondInput <= 0 || thirdInput <= 0 ) {
+      $("#notTri").show();
+    } else if ( firstInput === secondInput && secondInput === thirdInput ) {
+      $("#equal").show();
+    } else if ( firstInput === secondInput || secondInput === thirdInput ) {
+      $("#isos").show();
+    } else if ( firstInput !== secondInput && secondInput !== thirdInput ) {
+      $("#scal").show();
+    } else {
+      $("#notTri").show();
+    }
+    event.preventDefault();
   });
 });
--- src/triangle.js
+++ src/triangle.js
@@ -1,15 +1,15 @@
-export function triangle(firstInput, secondInput, thirdInput) {
-
-
-  if ( firstInput <= 0 || secondInput <= 0 || thirdInput <= 0 ) {
-    $("#notTri").show();
-  } else if ( firstInput === secondInput && secondInput === thirdInput ) {
-    $("#equal").show();
-  } else if ( firstInput === secondInput || secondInput === thirdInput ) {
-    $("#isos").show();
-  } else if ( firstInput !== secondInput && secondInput !== thirdInput ) {
-    $("#scal").show();
-  } else {
-    $("#notTri").show();
-  }
-}
+// export function triangle(firstInput, secondInput, thirdInput) {
+//
+//
+//   if ( firstInput <= 0 || secondInput <= 0 || thirdInput <= 0 ) {
+//     $("#notTri").show();
+//   } else if ( firstInput === secondInput && secondInput === thirdInput ) {
+//     $("#equal").show();
+//   } else if ( firstInput === secondInput || secondInput === thirdInput ) {
+//     $("#isos").show();
+//   } else if ( firstInput !== secondInput && secondInput !== thirdInput ) {
+//     $("#scal").show();
+//   } else {
+//     $("#notTri").show();
+//   }
+// }
